5a8b74798ca10d7da8fb599953003bebd5e1e1f0
Andres Felipe <felipephez@gmail.com>
2026-05-01 14:12:57 -0400

    refactor(cart): update ICartItemService signature for remove method
    - Add productId parameter to remove(Long userId, Long productId)

--- src/main/java/com/stockflow/backend/cart/service/ICartItemService.java
+++ src/main/java/com/stockflow/backend/cart/service/ICartItemService.java
@@ -10,8 +10,7 @@ import com.stockflow.backend.cart.dto.CartItemRequest;
 public interface ICartItemService {
 	
 	public List<CartItemRequest> addToCart(Long userId, List<CartItemRequest> cars);
-	
 	public List<CartItemRequest> getCart(Long userId);
-	
 	public void clearCart(Long userId);
+	public CartItemRequest remove(Long userId, Long productId);
 }
